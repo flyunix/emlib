98a2fb4a504fa8bde05041dc6472a6602a373015
lhl <lhl@zpd.com>
2018-12-25 09:11:31 +0800

    merge README.md

---
+++ CMakeFiles/emlib.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/mnt/hgfs/programmerGrowPlan/emlib/src/lock.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/src/lock.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/src/log.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/src/log.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/src/nest_lock.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/src/nest_lock.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/src/pthread_lock.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/src/pthread_lock.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/src/pthread_nest_lock.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/src/pthread_nest_lock.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/test/main.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/test/main.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/test/pthread_mutex.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/test/pthread_mutex.c.o"
+  "/mnt/hgfs/programmerGrowPlan/emlib/test/test.c" "/mnt/hgfs/programmerGrowPlan/emlib/CMakeFiles/emlib.dir/test/test.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "./include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/emlib.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/emlib.dir/src/lock.c.o"
+  "CMakeFiles/emlib.dir/src/log.c.o"
+  "CMakeFiles/emlib.dir/src/nest_lock.c.o"
+  "CMakeFiles/emlib.dir/src/pthread_lock.c.o"
+  "CMakeFiles/emlib.dir/src/pthread_nest_lock.c.o"
+  "CMakeFiles/emlib.dir/test/main.c.o"
+  "CMakeFiles/emlib.dir/test/pthread_mutex.c.o"
+  "CMakeFiles/emlib.dir/test/test.c.o"
+  "emlib.pdb"
+  "emlib"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/emlib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
